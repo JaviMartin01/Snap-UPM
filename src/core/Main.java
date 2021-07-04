2e7c4c3be95085cccbabd14d1a0bcae11bb86fa9
bq0298 <javier.mescobar@alumnos.upm.es>
2021-07-04 15:53:37 +0200

    Main commit

--- src/core/Main.java
+++ src/core/Main.java
@@ -2,6 +2,7 @@ package core;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Comenzando demo de SnapUPM....\n\n\n");
 		PruebaAceptacion pA = new PruebaAceptacion();
 	}
 }
